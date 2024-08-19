c4db74a82abd1c9b231af1f433597e27a069f8ea
SN-F-QR <43869889+SN-F-QR@users.noreply.github.com>
2024-08-19 21:52:25 +0800

    完成03Narrow Trail

--- projects/unions-and-literals/the-narrow-trail/src/index.ts
+++ projects/unions-and-literals/the-narrow-trail/src/index.ts
@@ -1,3 +1,47 @@
 export function runCommands() {
-	// Declare your variables and runtime logic here! ✨
+	// 枚举可以换成字符串联合类型
+	// nextSupply: "food" | "water" | undefined;
+	enum Res {
+		WATER,
+		FOOD,
+		UNDEFINE,
+	}
+	let avaliableRes: Res = Res.UNDEFINE;
+	const reduce: number = 1;
+	let food: number = 5;
+	let water: number = 5;
+	for (let day = 1; day <= 7; day += 1) {
+		const roll: number = Math.floor(Math.random() * 6) + 1;
+		if (roll === 1) {
+			avaliableRes = Res.FOOD;
+		} else if (roll === 2) {
+			avaliableRes = Res.WATER;
+		} else {
+			switch (avaliableRes) {
+				case Res.UNDEFINE:
+					if (roll % 2 === 0) {
+						avaliableRes = Res.FOOD;
+					} else {
+						avaliableRes = Res.WATER;
+					}
+					break;
+				case Res.FOOD:
+					food += roll;
+					avaliableRes = Res.UNDEFINE;
+					break;
+				case Res.WATER:
+					water += roll;
+					avaliableRes = Res.UNDEFINE;
+					break;
+			}
+		}
+
+		food -= reduce;
+		water -= reduce;
+
+		if (food === 0 || water === 0) {
+			return false;
+		}
+	}
+	return true;
 }
